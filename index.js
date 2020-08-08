015247f9db71c805cc446fa200ff9459fb0a72da
Kshitij Saini <k1941996@bitbucket.org>
2020-08-08 21:49:56 +0530

    Last Message functionality added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import  axios  from 'axios'
 import Paho from 'paho-mqtt'
-var username;
-var password;
+var username = "";
+var password = "";
 var AIRTRIK_KEY = ""
 var AIRTRIK_apiEndPoint = "https://www.airtrik.com/iot/";
+var lastMsgEndPoint = "https://airtrik.com/api/lastmsg/"
 var AIRTRIK_mqttEndPoint = "airtrik.com";
 var AIRTRIK_clientId = Math.random().toString(36).substring(2);
 var AIRTRIK_portNumber = 8083
@@ -30,8 +31,39 @@ export function send(deviceId, msg){
 	message.destinationName = AIRTRIK_KEY+"/"+deviceId;
 	AIRTRIK_client.send(message);
 }
+export function lastmsg(key = 0){
+  var key_data;
+  if(key == 0 && AIRTRIK_KEY === ""){
+    console.log("Airtrik Error: No key present, Cannot Continue")
+    return -1;
+  }
+  else if(AIRTRIK_KEY.length != 0 ){
+    key_data = `key=${AIRTRIK_KEY}`
+  }
+  else if( key  !=0){
+    key_data = `key=${key}`
+  }
+  axios.post(lastMsgEndPoint,key_data).then(res =>{
+    // console.log("res " , res)
+    return res.data  
+  }).then(res =>{
+    if(res.hasOwnProperty("error")){
+      console.log("Airtrik Error : ",res.error)
+      return res.error;
+    }
+    console.log(res)
+    return res;
+    
+  })
 
-export function init(key,onConnect=null,onReceive=null){
+}
+
+export function init(key = 0 ,onConnect=null,onReceive=null){
+  if(key == 0){
+    console.log("Airtrik Error : Key not specified Cannot Continue")
+    return;
+  }
+
   if(onConnect==null){
     onConnect = onConnectPredefinedUniqueFunction;
   }
@@ -42,16 +74,22 @@ export function init(key,onConnect=null,onReceive=null){
   var key_data = `key=${key}`
   axios.post(AIRTRIK_apiEndPoint,key_data)
   .then(res => {
+    // console.log("res ============ ",res)
+    if(res.data.hasOwnProperty("error")){
+      console.log("Airtrik Error : ",res.data.error)
+    }
     return res.data  
   }).then(res =>{
-      username = res.username
-      password = res.password
-      AIRTRIK_client.connect({onSuccess:onConnect, userName: username, password: password, useSSL: true});
-      AIRTRIK_client.onConnectionLost = onConnectionLost;
-      AIRTRIK_client.onMessageArrived = function onMessageArrived(message){
-        let msg = message.payloadString
-        let deviceId = message.destinationName.split("/")[1]
-        onReceive(msg, deviceId)
+      if(!res.hasOwnProperty("error")){
+        username = res.username
+        password = res.password
+        AIRTRIK_client.connect({onSuccess:onConnect, userName: username, password: password, useSSL: true});
+        AIRTRIK_client.onConnectionLost = onConnectionLost;
+        AIRTRIK_client.onMessageArrived = function onMessageArrived(message){
+          let msg = message.payloadString
+          let deviceId = message.destinationName.split("/")[1]
+          onReceive(msg, deviceId)
+        }
       }
     }
   )
